b899422dd0c1f900d31ba6d03858d3370d91ebd3
ChoKiyeon <qsky8520@gmail.com>
2021-02-24 23:24:49 +0900

    Unity

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -7,9 +7,15 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int age;
-        age = 30;
-        Debug.Log(age);
+        int answer;
+        answer = 3 - 4;
+        Debug.Log(answer);
+
+        answer = 5 * 6;
+        Debug.Log(answer);
+
+        answer = 8 / 4;
+        Debug.Log(answer);
     }
 
     // Update is called once per frame
